ff7568030570ba5646c5a20d5dd50e2b010f2a39
niuqaoJ-life <131826032+niuqaoJ-life@users.noreply.github.com>
2024-07-26 10:51:56 +1200

    Font weight of headings V1 finished

---
+++ v1 (used previous Blog App)/app.py
@@ -0,0 +1,8 @@
+# Imports the create_app function from the website folder/package
+from website import create_app
+
+# create_app function creates flask app instance
+# flask app runs in debug mode, which can provide error messages when bug is found
+if __name__ == "__main__":
+    app = create_app() 
+    app.run(debug=True) 
---
+++ v1 (used previous Blog App)/website/__init__.py
@@ -0,0 +1,44 @@
+# Importing necessary modules from flask 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+# Creates an SQLAlchemy object for the database
+db = SQLAlchemy()
+DB_NAME = "database.db" # Assigns the name for the database file
+
+# Function creates the flask app and configures it 
+def create_app():
+    app = Flask(__name__) # Creates a flask instance
+    app.config['SECRET_KEY'] = "7QmMbkyHfp" # Secret key configuration
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' # Database URI set to use SQLite
+    db.init_app(app)
+    
+    # Imports and register the blueprints for different parts of forum website app 
+    # imports views blueprints from python file
+    # imports auth blueprints from python file
+    from .views import views 
+    from .auth import auth 
+    
+    app.register_blueprint(views, url_prefix="/") # registers views blueprint
+    app.register_blueprint(auth, url_prefix="/") # registers auth blueprint
+    
+    # From models python file, takes user and the post. 
+    from .models import User, Post
+    
+    # Creates tables in the database if not existing 
+    with app.app_context():
+        db.create_all()
+    
+    #Login manager for user authentification. 
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login" 
+    login_manager.init_app(app)
+    
+    # Defines user loader callback for the login manager
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id)) # Returns user with their ID
+    
+    return app # returns configured flask app instance
