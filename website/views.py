e1f08c7d845f819cce0bf41db926684342044718
niuqaoJ-life <joaquin.rbadenas@gmail.com>
2024-08-16 00:32:34 +1200

    Added paginating for reviews

--- website/views.py
+++ website/views.py
@@ -17,7 +17,8 @@ def home():
 @views.route("/review")
 @login_required
 def review():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_created.desc()).paginate(page=page, per_page=4)
     return render_template("review.html", user=current_user, posts=posts)
 
 # CREATE POST
