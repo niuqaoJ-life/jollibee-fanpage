ab29412560d51530c8a5c336191725179c9f3812
niuqaoJ-life <joaquin.rbadenas@gmail.com>
2024-08-26 17:01:57 +1200

    Made init file pep8 compliant

--- website/__init__.py
+++ website/__init__.py
@@ -1,23 +1,36 @@
+"""Initializes the Flask application and configures with necessary modules etc.
+
+Functions:
+- create_app(): Creates and configures the Flask app,
+initializes the database, registers blueprints,
+and sets up user authentication.
+"""
+
 # Importing necessary modules from flask
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
 from flask_login import LoginManager
 
 # Creates an SQLAlchemy object for the database
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 # Function creates the flask app and configures it
-
-
 def create_app():
+    """Create and configures the Flask application.
+
+    Returns:
+        app (Flask): The configured Flask application.
+    Raises:
+        None
+    """
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "7QmMbkyHfp"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
-    # Imports and register the blueprints for different parts of forum website app
+    # Imports and register the blueprints for parts website
     # imports views blueprints from python file
     # imports auth blueprints from python file
     from .views import views
@@ -27,7 +40,7 @@ def create_app():
     app.register_blueprint(auth, url_prefix="/")
 
     # From models python file, takes user and the post.
-    from .models import User, Post
+    from .models import User
 
     # Creates tables in the database if not existing
     with app.app_context():
