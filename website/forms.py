b51358aae5ba61c7cef4cd7f25afde64272d5bfa
niuqaoJ-life <joaquin.rbadenas@gmail.com>
2024-08-29 18:27:02 +1200

    added minimum length passwords are to be

--- website/forms.py
+++ website/forms.py
@@ -29,19 +29,14 @@ class RegistrationForm(FlaskForm):
         validate_username: Validates if the entered username is already taken.
         validate_email: Validates if the entered email is already taken.
     """
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)],
-                           render_kw={"placeholder": "Username"})
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()],
-                        render_kw={"placeholder": "Email"})
-    password = PasswordField('Password',
-                             validators=[DataRequired()],
-                             render_kw={"placeholder": "Password"})
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[
-                                         DataRequired(), EqualTo('password')],
-                                     render_kw={"placeholder": "Confirm Password"})
+    username = StringField('Username', validators=[DataRequired(), Length(
+        min=2, max=20)], render_kw={"placeholder": "Enter Username"})
+    email = StringField('Email', validators=[DataRequired(), Email()], render_kw={
+                        "placeholder": "Enter Email Address"})
+    password = PasswordField('Password', validators=[DataRequired(), Length(
+        min=8)], render_kw={"placeholder": "Enter Password"})
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(
+    ), EqualTo('password')], render_kw={"placeholder": "Confirm Password"})
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
@@ -144,7 +139,7 @@ class ChangePasswordForm (FlaskForm):
     old_password = PasswordField('Old Password', validators=[
                                  DataRequired()], render_kw={"placeholder": "Old Password"})
     new_password = PasswordField('New Password', validators=[
-                                 DataRequired()], render_kw={"placeholder": "New Password"})
+                                 DataRequired(), Length(min=8)], render_kw={"placeholder": "New Password"})
     confirm_password = PasswordField('Confirm New Password', validators=[DataRequired(
     ), EqualTo('new_password')], render_kw={"placeholder": "Confirm New Password"})
     submit = SubmitField('Change Password')
