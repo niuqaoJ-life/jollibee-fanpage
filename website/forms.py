a971b0c2977357452efa354c75fdf9c325a5eaa5
niuqaoJ-life <joaquin.rbadenas@gmail.com>
2024-09-19 11:06:55 +1200

    Further debugging for sign-up page

--- website/forms.py
+++ website/forms.py
@@ -7,7 +7,7 @@ updating user account information, and changing passwords.
 from flask_wtf import FlaskForm
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Username
 from .models import User
 
 
@@ -29,7 +29,7 @@ class RegistrationForm(FlaskForm):
         validate_username: Validates if the entered username is already taken.
         validate_email: Validates if the entered email is already taken.
     """
-    username = StringField('Username', validators=[DataRequired(), Length(
+    username = StringField('Username', validators=[DataRequired(), Username(), Length(
         min=2, max=20)], render_kw={"placeholder": "Enter Username"})
     email = StringField('Email', validators=[DataRequired(), Email()], render_kw={
                         "placeholder": "Enter Email Address"})
